27f131c0f3552a62c413da40f83dca0cd0c6e083
ardrass <Ardrass@mail.ru>
2022-11-23 23:15:07 +0500

    Изменения для задачи 1(круги рандомного размера желтого цвета)

--- main.py
+++ main.py
@@ -3,36 +3,35 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 from random import randint
-import UI
+from PyQt5 import uic
 
 
-class YellowOvals(QMainWindow, UI.Ui_MainWindow):
+class YellowOvals(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.paint = False
-        self.pushButton.clicked.connect(self.paintEvent)
+        uic.loadUi('UI.ui', self)
+        self.qp = None
+        self.flag = False
+        self.pushButton.clicked.connect(self.button_clicked)
 
     def paintEvent(self, event):
-        if self.paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw(qp)
-            qp.end()
-            self.paint = False
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw(self.qp)
+            self.qp.end()
 
     def draw(self, qp):
-        if self.paint:
-            qp.setBrush(QColor("yellow"))
-            count = randint(3, 10)
-            for i in range(count):
-                x, y = randint(0, self.size().width()), randint(0, self.size().height())
-                w = h = randint(5, 200)
-                qp.drawEllipse(x, y, w, w)
+        qp.setBrush(QColor("yellow"))
+        count = randint(3, 10)
+        for i in range(count):
+            x, y = randint(0, self.size().width()), randint(0, self.size().height())
+            w = h = randint(5, 200)
+            qp.drawEllipse(x, y, w, w)
 
     def button_clicked(self):
-        self.paint = True
-        self.repaint()
+        self.flag = True
+        self.update()
 
 
 if __name__ == '__main__':
